b53a04273fb5eeaf81e1c6f899d8fbc9c905ba6f
bryn9696 <bryn2009@live.co.uk>
2022-09-20 14:37:53 +0100

    primitive types variables

--- commandlineapp/helloworld/Main.java
+++ commandlineapp/helloworld/Main.java
@@ -2,6 +2,17 @@ package commandlineapp.helloworld;
 
 public class Main {
   public static void main(String[] args) {
+    int bill = 260_187_345;
+    byte age = 26;
+    double price = 6.54;
+    float amount = 7.66F;
+    String name = "Bryn";
+
     System.out.println("Hello World");
+    System.out.println(name + " is " + age);
+    System.out.println(bill);
+    System.out.println(price);
+    System.out.println(amount);
+
   }
 }
