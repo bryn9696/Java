624e6d2cb33ab0bac4361b1a2f601c41e43a7262
bryn9696 <bryn2009@live.co.uk>
2022-09-20 14:49:00 +0100

    reference types variables

--- commandlineapp/helloworld/Main.java
+++ commandlineapp/helloworld/Main.java
@@ -1,5 +1,7 @@
 package commandlineapp.helloworld;
 
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
     int bill = 260_187_345;
@@ -7,12 +9,14 @@ public class Main {
     double price = 6.54;
     float amount = 7.66F;
     String name = "Bryn";
+    Date now = new Date();
 
     System.out.println("Hello World");
     System.out.println(name + " is " + age);
     System.out.println(bill);
     System.out.println(price);
     System.out.println(amount);
+    System.out.println(now);
 
   }
 }
