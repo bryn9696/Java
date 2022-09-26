324623007af0fe05d925af317e145651c2c2bc61
bryn9696 <bryn2009@live.co.uk>
2022-09-26 11:16:34 +0100

    exploring arrays

--- commandlineapp/helloworld/Main.java
+++ commandlineapp/helloworld/Main.java
@@ -1,5 +1,6 @@
 package commandlineapp.helloworld;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -11,6 +12,13 @@ public class Main {
     String name = "Bryn";
     String sentence = "   Hi There \"Bryn\" \n \t\\filename";
     Date now = new Date();
+    int[] numbers = new int[5];
+    numbers[0] = 2;
+    numbers[1] = 4;
+    int[] numbers2 = { 2, 4, 6, 8, 10 };
+    int[][] numbers3 = new int[2][3];
+    numbers3[1][1] = 1;
+    int[][] numbers4 = { {2, 4, 6}, {8, 10, 12}, {14, 16, 18} };
 
     System.out.println("Hello World");
     System.out.println(name + " is " + age);
@@ -19,6 +27,10 @@ public class Main {
     System.out.println(amount);
     System.out.println(now);
     System.out.println(sentence.trim());
+    System.out.println(Arrays.toString(numbers));
+    System.out.println(numbers2.length);
+    System.out.println(Arrays.deepToString(numbers3));
+    System.out.println(Arrays.deepToString(numbers4));
 
   }
 }
