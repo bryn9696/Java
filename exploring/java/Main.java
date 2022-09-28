b41a6411034f09798b02e4a0c55727d557429a3f
bryn9696 <bryn2009@live.co.uk>
2022-09-28 10:21:05 +0100

    basic math

--- exploring/java/Main.java
+++ exploring/java/Main.java
@@ -3,6 +3,11 @@ package exploring.java;
 public class Main {
   public static void main(String[] args) {
     final float PI = 3.14F;
-     
+    System.out.println(PI);
+    double number = (double)10 / (double)3;
+    System.out.println(number);
+    int x = 1;
+    x++;
+    System.out.println(x);
   }
 }
