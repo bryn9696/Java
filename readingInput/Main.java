1b4af986fd5e0b0964661fb0916098d9e1a50761
bryn9696 <bryn2009@live.co.uk>
2022-09-30 10:47:52 +0100

    reading input

--- readingInput/Main.java
+++ readingInput/Main.java
@@ -1,7 +1,11 @@
 package readingInput;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    byte age = scanner.nextByte();
+    System.out.println("you are " + age);
   }
 }
