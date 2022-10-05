e4f38054fbf29c9560de26eb68634d6eb2aff9f5
bryn9696 <bryn2009@live.co.uk>
2022-10-05 16:21:38 +0100

    simplified if statement

--- IfStatements/Main.java
+++ IfStatements/Main.java
@@ -2,12 +2,15 @@ package IfStatements;
 
 public class Main {
   public static void main(String[] args) {
-    int temp = 2;
+    int temp = 17;
+    boolean hot = temp > 15;
     if (temp > 25) {
       System.out.println("It's damn hot");
       System.out.println("Get a drink");
-    } else if (temp > 18 && temp <= 25) {
+    } else if (temp > 15) {
       System.out.println("great day");
     } else System.out.println("it do be cold");
+
+    System.out.println(hot);
   }
 }
